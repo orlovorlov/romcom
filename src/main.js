fa65b292f99fa91fdde256312fe38b050bd98682
Dylan Duke <laytonmaes@gmail.com>
2022-02-11 19:34:36 -0700

    Add form functionality

--- src/main.js
+++ src/main.js
@@ -11,10 +11,16 @@ var buttonRandomCover = document.querySelector('.random-cover-button')
 var buttonHome = document.querySelector('.home-button');
 var buttonSaveCover = document.querySelector('.save-cover-button');
 var buttonViewSaved = document.querySelector('.view-saved-button');
+var buttonCreateNewBook = document.querySelector('.create-new-book-button')
+// these are our forms
 var formShow = document.querySelector('.form-view')
 var homeView = document.querySelector('.home-view')
 var savedView = document.querySelector('.saved-view')
-
+// these are our inputs
+var textFieldCover = document.querySelector('#cover')
+var textFieldTitle = document.querySelector('#title')
+var textFieldDescriptor1 = document.querySelector('#descriptor1')
+var textFieldDescriptor2 = document.querySelector('#descriptor2')
 
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -26,7 +32,7 @@ buttonViewSaved.addEventListener('click', viewSavedCovers)
 buttonMakeNewCover.addEventListener('click', viewForm)
 buttonRandomCover.addEventListener("click", randomizeCover)
 buttonHome.addEventListener('click', viewHome)
-
+buttonCreateNewBook.addEventListener('click', createNewBook)
 // Create your event handlers and other functions here 👇
 
 
@@ -35,6 +41,7 @@ buttonHome.addEventListener('click', viewHome)
 //   //
 // formShow
 // }
+
 function viewHome() {
   show(homeView);
   show(buttonRandomCover);
@@ -79,10 +86,34 @@ function randomizeCover() {
   coverTitle.innerText = currentCover.title;
   tagLine1.innerText =  currentCover.tagline1;
   tagLine2.innerText = currentCover.tagline2;
-
-
   //remember to remove CL
-  console.log(currentCover)
+//   console.log(currentCover)
+//
+}
+
+// function saveFormFields() {
+//   covers.push(textFieldCover);
+//   titles.push(textFieldTitle);
+//   descriptors.push(textFieldDescriptor1);
+//   descriptors.push(textFieldDescriptor2);
+//   console.log(titles)
+// }
+
+function createNewBook(event) {
+  event.preventDefault()
+  currentCover = new Cover()
+
+  coverImage.src = textFieldCover.value;
+  covers.push(coverImage.src);
+  coverTitle.innerText = textFieldTitle.value;
+  titles.push(coverTitle.innerText);
+  tagLine1.innerText = textFieldDescriptor1.value;
+  descriptors.push(tagLine1.innerText);
+  tagLine2.innerText = textFieldDescriptor2.value;
+  descriptors.push(tagLine2.innerText);
+
+  console.log(titles, covers, descriptors)
+  viewHome()
 
 }
 
